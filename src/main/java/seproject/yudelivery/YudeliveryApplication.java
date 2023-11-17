b7a62330e5e1b18a3a261c7ef964dd7bd89a2e1d
yeosu623 <yeosu623@naver.com>
2023-11-17 16:55:36 +0900

    .

--- src/main/java/seproject/yudelivery/YudeliveryApplication.java
+++ src/main/java/seproject/yudelivery/YudeliveryApplication.java
@@ -9,7 +9,7 @@ public class YudeliveryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YudeliveryApplication.class, args);
 		//임구철
-		//정수열
+		//정수열1231234
 		//서예주
 		//신은석
 		//손병하1231234
