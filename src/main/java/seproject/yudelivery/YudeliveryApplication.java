004f5d59c19e97434745fa0f7622e3f33d4f13e8
yeosu623 <yeosu623@naver.com>
2023-11-17 16:11:44 +0900

    개인 프랜치에서 병합 테스트

--- src/main/java/seproject/yudelivery/YudeliveryApplication.java
+++ src/main/java/seproject/yudelivery/YudeliveryApplication.java
@@ -9,7 +9,7 @@ public class YudeliveryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YudeliveryApplication.class, args);
 		//임구철
-		//정수열
+		//정수열 + 개인 브랜치에서 병합 테스트
 	}
 
 }
