ffffbe30d0f56ce3d990d0cf8435c010f4003f7e
gooch <gooch123@naver.com>
2023-11-17 16:28:23 +0900

    테스트2

--- src/main/java/seproject/yudelivery/YudeliveryApplication.java
+++ src/main/java/seproject/yudelivery/YudeliveryApplication.java
@@ -8,8 +8,9 @@ public class YudeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YudeliveryApplication.class, args);
-		//임구철
+		//임구철222
 		//정수열
+
 	}
 
 }
