dd37703abc719dc3a9cd434b817d290955735b94
yeosu623 <yeosu623@naver.com>
2023-11-17 16:42:20 +0900

    .

--- src/main/java/seproject/yudelivery/YudeliveryApplication.java
+++ src/main/java/seproject/yudelivery/YudeliveryApplication.java
@@ -8,9 +8,6 @@ public class YudeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YudeliveryApplication.class, args);
-		//임구철
-		//정수열 개인 브랜치에서 master로 머지 완료
-		//서예주
 	}
 
 }
