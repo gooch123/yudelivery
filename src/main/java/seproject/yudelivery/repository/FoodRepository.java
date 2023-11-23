6ba36dd32b560d6577fabc823e6c1669a4496fde
서예주 <yellu67@yu.ac.kr>
2023-11-24 00:30:34 +0900

    FoodRepository 메소드 추가

--- src/main/java/seproject/yudelivery/repository/FoodRepository.java
+++ src/main/java/seproject/yudelivery/repository/FoodRepository.java
@@ -1,6 +1,7 @@
 package seproject.yudelivery.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import seproject.yudelivery.entity.FoodEntity;
@@ -8,5 +9,19 @@ import seproject.yudelivery.entity.FoodEntity;
 @Repository
 public interface FoodRepository extends JpaRepository<FoodEntity, Long> {
     Optional<FoodEntity> findById(Long id);
-    // 다른 필요한 메서드
+
+    @Query("SELECT f FROM FoodEntity f WHERE f.food_name = :foodName")
+    Optional<FoodEntity> findFoodExisting(String foodName); // 음식이 이미 존재하는지 확인하는 메서드
+
+    default FoodEntity saveNewFood(FoodEntity food) {
+        return save(food); // 새로운 음식 저장
+    }
+
+    default void deleteFood(Long id) {
+        deleteById(id); // 음식 삭제
+    }
+
+    default FoodEntity updateFood(FoodEntity food) {
+        return save(food); // 음식 업데이트
+    }
 }
