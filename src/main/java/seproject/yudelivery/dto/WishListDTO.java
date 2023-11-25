5546ddf95c1f725c6547de0fc94e475ec7828ab7
gooch <gooch123@naver.com>
2023-11-25 21:08:42 +0900

    찜목록 기능 일부 구현

--- src/main/java/seproject/yudelivery/dto/WishListDTO.java
+++ src/main/java/seproject/yudelivery/dto/WishListDTO.java
@@ -1,11 +1,16 @@
 package seproject.yudelivery.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
+@Getter
+@ToString
 public class WishListDTO {
 
     private String storeName;
     private Long storeId;
+    private String storeInfo;
 
 }
