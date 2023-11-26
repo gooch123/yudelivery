1b7a0c38565e067bcf668cbcf81dcb4671151c6e
gooch <gooch123@naver.com>
2023-11-26 21:11:40 +0900

    fix 1

--- src/main/java/seproject/yudelivery/controller/BasketController.java
+++ src/main/java/seproject/yudelivery/controller/BasketController.java
@@ -5,10 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import seproject.yudelivery.dto.BasketDTO;
-import seproject.yudelivery.entity.UserEntity;
+import seproject.yudelivery.entity.StoreEntity;
 import seproject.yudelivery.service.BasketService;
+import seproject.yudelivery.service.StoreService;
 
 import java.util.List;
 
@@ -18,6 +21,7 @@ import java.util.List;
 public class BasketController {
 
     private final BasketService basketService;
+    private final StoreService storeService;
 
     @GetMapping("/main")
     public String basketHome(Model model, HttpSession httpSession){
@@ -29,9 +33,34 @@ public class BasketController {
 //        long userId = user.getId();
         Long userId = 1L;
         List<BasketDTO> basketDTOList = basketService.getBasketList(userId);
+        int totalPrice = basketService.getTotalPrice(userId);
+        String basketStoreName = basketService.getBasketStoreName(userId);
+
+        model.addAttribute("totalPrice",totalPrice);
         model.addAttribute("list",basketDTOList);
+        model.addAttribute("store",basketStoreName);
+
 
         return "basket/main";
     }
+
+    @PostMapping("/{id}/cancel")
+    public String cancel(@PathVariable(name = "id") Long basketFoodId){
+        basketService.cancelFood(basketFoodId);
+        return "redirect:/basket/main";
+    }
+
+    @PostMapping("/{id}/add")
+    public String addFoodQuantity(@PathVariable(name = "id") Long basketFoodId){
+        basketService.updateBasketFoodQuantity(basketFoodId,1);
+        return "redirect:/basket/main";
+    }
+
+    @PostMapping("/{id}/sub")
+    public String subFoodQuantity(@PathVariable(name = "id") Long basketFoodId){
+        basketService.updateBasketFoodQuantity(basketFoodId,-1);
+        return "redirect:/basket/main";
+    }
+
 
 }
