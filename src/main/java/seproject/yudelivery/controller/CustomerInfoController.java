930f054354eca29463052232e32fb59b1990f685
gooch <gooch123@naver.com>
2023-11-28 19:26:23 +0900

    주문 내역 기능

--- src/main/java/seproject/yudelivery/controller/CustomerInfoController.java
+++ src/main/java/seproject/yudelivery/controller/CustomerInfoController.java
@@ -4,9 +4,10 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import seproject.yudelivery.dto.OrderDTO;
+import org.springframework.web.bind.annotation.*;
+import seproject.yudelivery.dto.OrderFoodDTO;
+import seproject.yudelivery.dto.OrderViewDTO;
+import seproject.yudelivery.entity.UserEntity;
 import seproject.yudelivery.service.OrderService;
 
 import java.util.List;
@@ -19,21 +20,31 @@ public class CustomerInfoController {
     private final OrderService orderService;
 
     @GetMapping("/orderList")
-    public String orderList(HttpSession httpSession, Model model){
-//        UserEntity user = (UserEntity) httpSession.getAttribute("user");
-//        if(user == null){
-//            return "home"; //로그인이 안되어있으면 돌려보냄
-//            // 유저 타입이 맞지 않아도 home 으로 돌려보냄
+    public String orderList(@SessionAttribute(name = "user",required = false) UserEntity user, Model model){
+//        if(user == null || user.getRole() != UserRole.CUSTOMER){
+//            return null;
 //        }
 //        Long userId = user.getId();
 
         Long userId = 1L;
 
-        List<OrderDTO> orderList = orderService.getOrdersByUserId(userId);
+        List<OrderViewDTO> orderList = orderService.getOrderViewList(userId);
         model.addAttribute("orderList",orderList);
-        return null;
+        return "info/orderList";
     }
 
-
+    @GetMapping("/orderDetail/{id}")
+    public String orderDetail(
+            Model model,
+            @SessionAttribute(name = "user",required = false) UserEntity user,
+            @PathVariable("id") Long orderId){
+//        if(user == null || user.getRole() != UserRole.CUSTOMER){
+//            return null;
+//        }
+//        Long userId = user.getId();
+        List<OrderFoodDTO> orderFoods = orderService.getOrderFoods(orderId);
+        model.addAttribute("orderFoods",orderFoods);
+        return "info/orderDetail";
+    }
 
 }
