3bb8658df66a285a8d7be9c626982b6675de94c5
eunsu <eunsuk279@naver.com>
2023-11-28 18:06:18 +0900

    store

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-insert into user_entity (user_id,username, password,nickname,phone,email,dtype) values ('admin', 'admin','admin','admin', '010','admin','store');
+insert into user_entity(user_id,username, password,nickname,phone,email,dtype) values ('admin', 'admin','admin','admin', '010','admin','store');
 insert into store_entity(user_id,store_name,address1,address2,address3,category,phone,open_time,close_time,deliver_time,store_info,sales) values ('1','admin','admin','admin','admin','admin','admin','20:00','20:00','30','admin','50');
+insert into food_entity(store_id,food_name,food_price,food_info,food_size) values ('1','beak ban','30000','beak ban jeon sik','2');
