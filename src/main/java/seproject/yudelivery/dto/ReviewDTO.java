d3d9878dbbbdffe1fc4efd31873e4adecb6b56a5
서예주 <yellu67@yu.ac.kr>
2023-11-30 07:58:00 +0900

    review

--- src/main/java/seproject/yudelivery/dto/ReviewDTO.java
+++ src/main/java/seproject/yudelivery/dto/ReviewDTO.java
@@ -2,37 +2,28 @@ package seproject.yudelivery.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import seproject.yudelivery.entity.CustomerEntity;
-import seproject.yudelivery.entity.ReviewEntity;
-import seproject.yudelivery.entity.StoreEntity;
-import seproject.yudelivery.repository.StoreRepository;
+import seproject.yudelivery.entity.*;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Getter
 public class ReviewDTO {
-    private Long id;
-    private Long store_id;
-    private Long customer_id;
+    private StoreEntity store;
+    private UserEntity customer;
+    private OrderEntity order;
     private String review_content;
     private Double review_starpoint;
     private String comment;
-    private final StoreRepository storeRepository;
 
-    public ReviewEntity toEntity(){
-        ReviewEntity reviewEntity = new ReviewEntity();
-        reviewEntity.setId(this.id);
-        reviewEntity.setReview_content(this.review_content);
-        reviewEntity.setReview_starpoint(this.review_starpoint);
-        reviewEntity.setComment(this.comment);
-
-        StoreEntity storeEntity = storeRepository.findStoreById(this.store_id);
-        reviewEntity.setStore(storeEntity);
-
-        /*customer해야함
-        CustomerEntity customerEntity = customerRepository.findCustomer(this.customer_id);
-        reviewEntity.setCustomer(customerEntity);
-         */
-
-        return reviewEntity;
+    public ReviewEntity toEntity() {
+        return ReviewEntity.builder()
+                .store(this.store)
+                .customer(this.customer)
+                //.order(this.order)
+                .review_content(this.review_content)
+                .review_starpoint(this.review_starpoint)
+                .comment(this.comment)
+                .build();
     }
 }
