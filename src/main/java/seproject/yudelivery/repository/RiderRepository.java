4c59ccf61e2b002d3b43043bcf3ab879e3f29a4d
LeeMinJae <ghtjdcc1@naver.com>
2023-11-30 13:03:02 +0900

    Rider Controller Rider Repository Rider Entity Rider Repository

--- src/main/java/seproject/yudelivery/repository/RiderRepository.java
+++ src/main/java/seproject/yudelivery/repository/RiderRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface RiderRepository extends JpaRepository<RiderEntity, Long> {
 
     // 주문자 ID로 RiderEntity 찾기
-    Optional<RiderEntity> findByCustomerId(Long customerId);
+    Optional<RiderEntity> findByOrderId(Long customerId);
 
     // 배달 상태를 '배달완료'로 변경
    void updateDeliveryStatusToDelivered(Long riderId);
