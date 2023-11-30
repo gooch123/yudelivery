a011e65027f52b5a8d491102058fc7cf77c1e0e1
LeeMinJae <ghtjdcc1@naver.com>
2023-11-30 21:05:08 +0900

    Rider Controller Rider Repository Rider Entity Rider Repository

--- src/main/java/seproject/yudelivery/controller/RiderController.java
+++ src/main/java/seproject/yudelivery/controller/RiderController.java
@@ -1,3 +1,4 @@
+// RiderController.java
 package seproject.yudelivery.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +16,20 @@ public class RiderController {
     private RiderRepository riderRepository;
 
     // 주문자 ID로 Rider 정보 가져오기
-    @GetMapping("/getByorderId/{orderId}")
-    public Optional<RiderEntity> getRiderByOrderId(@PathVariable Long orderId) {
-        return riderRepository.findByOrderId(orderId);
+    @GetMapping("/getByCustomerId/{customerId}")
+    public Optional<RiderEntity> getRiderByCustomerId(@PathVariable Long customerId) {
+        return riderRepository.findByCustomerId(customerId);
     }
 
     // 배달완료로 상태 변경
     @PutMapping("/completeDelivery/{riderId}")
     public void completeDelivery(@PathVariable Long riderId) {
-//        riderRepository.updateDeliveryStatusToDelivered(riderId);
+        Optional<RiderEntity> optionalRider = riderRepository.findById(riderId);
+        if (optionalRider.isPresent()) {
+            RiderEntity rider = optionalRider.get();
+            rider.setDeliveryStatus("DELIVERED");
+            riderRepository.save(rider);
+        }
     }
 
     // Add other CRUD operations as needed
--- src/main/java/seproject/yudelivery/repository/RiderRepository.java
+++ src/main/java/seproject/yudelivery/repository/RiderRepository.java
@@ -1,16 +1,17 @@
+// RiderRepository.java
 package seproject.yudelivery.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import seproject.yudelivery.entity.RiderEntity;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface RiderRepository extends JpaRepository<RiderEntity, Long> {
 
-    // 주문자 ID로 RiderEntity 찾기
-    List<RiderEntity> findAllByCustomerId(Long customerId);
+    Optional<RiderEntity> findByCustomerId(Long customerId);
 
-    // Optional<RiderEntity> findByCustomerId(Long customerId); // Alternatively, if you expect at most one result
+    // Optional<RiderEntity> findById(Long riderId); // Uncomment this if you need to find by rider ID
 
+    // 추가
+    void updateDeliveryStatusToDelivered(Long riderId);
 }
