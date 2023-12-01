ddadde269b03cbd7c8f863b0f80af729acb4bec0
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-12-01 19:18:04 +0900

    admin 최종 완성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,4 @@ insert into user_entity(user_id,username, password,nickname,phone,email,dtype) v
 insert into store_entity(user_id,store_name,address1,address2,address3,category,phone,open_time,close_time,deliver_time,store_info,sales) values ('1','admin','admin','admin','admin','admin','010-0000-0000','20:00','20:00','30','admin','50');
 insert into food_entity(store_id,food_name,food_price,food_info,food_size) values ('1','beak ban','30000','beak ban jeon sik','2');
 insert into review_entity(store_id, customer_id,review_content, review_starpoint, comment) values ('1','1','1','1','1'),('1','1','2','2',null);
-insert into admin_entity(review_id, content) values ('1','bad comment111');
-insert into admin_entity(review_id, content) values ('2','bad comment222');
 insert into rider_entity(rider_id,delivery_status,delivery_address1,delivery_address2,delivery_address3,phone) values ('1','complete','1','1','1','010-0000-0000');
