20393c440756be885b5de07f32291d20841bd1e7
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-12-07 17:34:51 +0900

    admin 리뷰처리 버그 수정

--- src/main/java/seproject/yudelivery/controller/AdminController.java
+++ src/main/java/seproject/yudelivery/controller/AdminController.java
@@ -12,7 +12,9 @@ import seproject.yudelivery.entity.UserEntity;
 import seproject.yudelivery.service.AdminService;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -37,12 +39,9 @@ public class AdminController {
      */
     @GetMapping("/review")
     public String getReview(Model model) {
-        List<AdminEntity> reportedReviews = adminService.findAllReview();
-        List<AdminEntity> badReviews = adminService.getAllBadReview();
-
-        List<AdminEntity> reviews = new ArrayList<>();
-        reviews.addAll(reportedReviews);
-        reviews.addAll(badReviews);
+        Set<AdminEntity> reviews = new HashSet<>();
+        reviews.addAll(adminService.findAllReview());
+        reviews.addAll(adminService.getAllBadReview());
 
         model.addAttribute("reviews", reviews);
         return "admin/review";
--- src/main/java/seproject/yudelivery/entity/AdminEntity.java
+++ src/main/java/seproject/yudelivery/entity/AdminEntity.java
@@ -14,8 +14,26 @@ public class AdminEntity {
     @Column(name = "review_id") // 리뷰의 id 외래키
     private Long id;
 
-
-
     @Column(name = "content")
     private String content;
+
+    @Override
+    public boolean equals(Object obj) {
+        AdminEntity adminEntity = (AdminEntity) obj;
+        if(this.id.equals(adminEntity.id) && this.content.equals(adminEntity.content)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hashCode = 1;
+
+        hashCode = prime * hashCode + ((content == null) ? 0 : content.hashCode());
+        hashCode = prime * hashCode + (int)id.longValue();
+
+        return hashCode;
+    }
 }
